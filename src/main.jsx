5c3a41df16ff3dfe1f7aa6dbb6ae5518b933e70c
Tawkir711 <mdtawkirk711@gmail.com>
2023-09-22 21:34:53 +0600

    added errorpage

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,24 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from './Components/Root/Root';
 import Home from './Components/Home/Home';
+import Applied from './Components/Applied/Applied';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage> ,
     children: [
       {
-        path: '/',
-        element:<Home></Home>,
-      }
-    ]
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/applied",
+        element: <Applied></Applied>
+      },
+    ],
   },
 ]);
 
